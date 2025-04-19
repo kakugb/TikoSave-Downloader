8cac7da5efda9190a0547db8afe64f696b22f238
kakugb <karamat.4101@gmail.com>
2025-04-19 13:14:41 -0700

    add more pages

---
+++ src/app/about/page.tsx
@@ -0,0 +1,7 @@
+function AboutUs(){
+    return(
+        <h1>About Us page</h1>
+    )
+}
+
+export default AboutUs
---
+++ src/app/contactus/page.tsx
@@ -0,0 +1,13 @@
+function ContactUs(){
+    return(
+        <>
+        <h1 className="text-5xl text-center text-teal-500 py-4 font-bold">Contact Us</h1>
+        <div className="w-7xl mx-auto">
+        <p className="text-xl ">If you have any questions or would like to report errors, please send us an email in English.</p>
+        <p className="text-xl  mt-4">Please check the FAQ page if you have a question. If reporting an error, please include details of error message or code in the email.</p>
+        <p className="text-xl  mt-4">The email address of SaveTT is support@savett.cc</p>
+        </div>
+        </>
+    )
+}
+export default ContactUs
